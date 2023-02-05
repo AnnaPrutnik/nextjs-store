2c56869bb9319123623ce2939ce54f95b60743d4
Anna Prutnik <prutnik.anna@gmail.com>
2023-02-05 17:12:00 +0200

    fix:change base url in axios instance

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
+const BASE_URL = process.env.SERVER_URL;
 const instance = axios.create({
-  baseURL: 'http://localhost:3000/',
+  // baseURL: 'http://localhost:3000/',
+  baseURL: BASE_URL,
   headers: { 'Content-Type': 'application/json' },
 });
 
